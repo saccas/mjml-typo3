eba401aa9fc419958f5af665f95a40b30bb28347
Daniel Siepmann <coding@daniel-siepmann.de>
2017-12-14 23:06:08 +0100

    TASK: Add first test
    Add all dependencies to run tests.
    Configure tests as composer task.
    Add tests to travis.
    Adjust code to make him exchangeable and testable.

---
+++ Classes/Domain/Renderer/Command.php
@@ -0,0 +1,45 @@
+<?php
+namespace Saccas\Mjml\Domain\Renderer;
+
+use TYPO3\CMS\Core\Utility\CommandUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class Command implements RendererInterface
+{
+    protected function getHtmlFromMjml($mjml)
+    {
+        $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mjml']);
+
+        $temporaryMjmlFileWithPath = GeneralUtility::tempnam('mjml_', '.mjml');
+
+        GeneralUtility::writeFileToTypo3tempDir($temporaryMjmlFileWithPath, $mjml);
+
+        // see https://mjml.io/download and https://www.npmjs.com/package/mjml-cli
+        $cmd = $configuration['nodeBinaryPath'] . ' ' . $configuration['mjmlBinaryPath'] . $configuration['mjmlBinary'];
+        $args = $configuration['mjmlParams'] . ' ' . $temporaryMjmlFileWithPath;
+
+        $result = [];
+        $returnValue = '';
+        CommandUtility::exec($this->getEscapedCommand($cmd, $args), $result, $returnValue);
+
+        GeneralUtility::unlink_tempfile($temporaryMjmlFileWithPath);
+
+        return implode('', $result);
+    }
+
+    /**
+     * @param string $cmd
+     * @param string $args
+     * @return string
+     */
+    protected function getEscapedCommand(string $cmd, string $args)
+    {
+        $escapedCmd = escapeshellcmd($cmd);
+
+        $argsArray = explode(' ', $args);
+        $escapedArgsArray = CommandUtility::escapeShellArguments($argsArray);
+        $escapedArgs = implode(' ', $escapedArgsArray);
+
+        return $escapedCmd . ' ' . $escapedArgs;
+    }
+}
--- Classes/View/MjmlBasedView.php
+++ Classes/View/MjmlBasedView.php
@@ -1,53 +1,29 @@
 <?php
-
 namespace Saccas\Mjml\View;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Mvc\Cli\Command;
+use Saccas\Mjml\Domain\Renderer\RendererInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
-use TYPO3\CMS\Core\Utility\CommandUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class MjmlBasedView extends StandaloneView
 {
+    /**
+     * @var RendererInterface
+     */
+    protected $renderer;
+
+    public function __construct(ContentObjectRenderer $contentObject = null, RendererInterface $renderer = null)
+    {
+        parent::__construct($contentObject);
+
+        $this->renderer = $renderer;
+        if ($this->renderer === null) {
+            $this->renderer = $this->objectManager->get(RendererInterface::class);
+        }
+    }
+
     public function render($actionName = null)
     {
-        return $this->getHtmlFromMjml(parent::render());
-    }
-
-    protected function getHtmlFromMjml($mjml)
-    {
-        $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mjml']);
-
-        $temporaryMjmlFileWithPath = GeneralUtility::tempnam('mjml_', '.mjml');
-
-        GeneralUtility::writeFileToTypo3tempDir($temporaryMjmlFileWithPath, $mjml);
-
-        // see https://mjml.io/download and https://www.npmjs.com/package/mjml-cli
-        $cmd = $configuration['nodeBinaryPath'] . ' ' . $configuration['mjmlBinaryPath'] . $configuration['mjmlBinary'];
-        $args = $configuration['mjmlParams'] . ' ' . $temporaryMjmlFileWithPath;
-
-        $result = [];
-        $returnValue = '';
-        CommandUtility::exec($this->getEscapedCommand($cmd, $args), $result, $returnValue);
-
-        GeneralUtility::unlink_tempfile($temporaryMjmlFileWithPath);
-
-        return implode('', $result);
-    }
-
-    /**
-     * @param string $cmd
-     * @param string $args
-     * @return string
-     */
-    private function getEscapedCommand(string $cmd, string $args)
-    {
-        $escapedCmd = escapeshellcmd($cmd);
-
-        $argsArray = explode(' ', $args);
-        $escapedArgsArray = CommandUtility::escapeShellArguments($argsArray);
-        $escapedArgs = implode(' ', $escapedArgsArray);
-
-        return $escapedCmd . ' ' . $escapedArgs;
+        return $this->renderer->getHtmlFromMjml(parent::render($actionName));
     }
 }
