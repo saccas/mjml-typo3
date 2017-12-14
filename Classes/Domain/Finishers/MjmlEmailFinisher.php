26aa55e3ff037c7cc28296e1c5e0c6b5c62019b0
Daniel Huf <huf.daniel@gmail.com>
2017-12-14 18:22:32 +0100

    [TASK] Remove duplicated code from extend

--- Classes/Domain/Finishers/MjmlEmailFinisher.php
+++ Classes/Domain/Finishers/MjmlEmailFinisher.php
@@ -15,68 +15,14 @@ namespace Saccas\Mjml\Domain\Finishers;
  * The TYPO3 project - inspiring people to share!
  */
 
-use TYPO3\CMS\Core\Mail\MailMessage;
-use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Form\Domain\Finishers\Exception\FinisherException;
-use TYPO3\CMS\Form\Domain\Model\FormElements\FileUpload;
 use TYPO3\CMS\Form\Domain\Runtime\FormRuntime;
-use TYPO3\CMS\Form\Service\TranslationService;
 use TYPO3\CMS\Form\ViewHelpers\RenderRenderableViewHelper;
 use Saccas\Mjml\View\MjmlBasedView;
 
-/**
- * This finisher sends an email to one recipient
- *
- * Options:
- *
- * - templatePathAndFilename (mandatory): Template path and filename for the mail body
- * - layoutRootPath: root path for the layouts
- * - partialRootPath: root path for the partials
- * - variables: associative array of variables which are available inside the Fluid template
- *
- * The following options control the mail sending. In all of them, placeholders in the form
- * of {...} are replaced with the corresponding form value; i.e. {email} as recipientAddress
- * makes the recipient address configurable.
- *
- * - subject (mandatory): Subject of the email
- * - recipientAddress (mandatory): Email address of the recipient
- * - recipientName: Human-readable name of the recipient
- * - senderAddress (mandatory): Email address of the sender
- * - senderName: Human-readable name of the sender
- * - replyToAddress: Email address of to be used as reply-to email (use multiple addresses with an array)
- * - carbonCopyAddress: Email address of the copy recipient (use multiple addresses with an array)
- * - blindCarbonCopyAddress: Email address of the blind copy recipient (use multiple addresses with an array)
- * - format: format of the email (one of the FORMAT_* constants). By default mails are sent as HTML
- *
- * Scope: frontend
- */
 class MjmlEmailFinisher extends \TYPO3\CMS\Form\Domain\Finishers\EmailFinisher
 {
-    const FORMAT_PLAINTEXT = 'plaintext';
-    const FORMAT_HTML = 'html';
-
-    /**
-     * @var array
-     */
-    protected $defaultOptions = [
-        'recipientName' => '',
-        'senderName' => '',
-        'format' => self::FORMAT_HTML,
-        'attachUploads' => true
-    ];
-
-    /**
-     * Executes this finisher
-     * @see AbstractFinisher::execute()
-     *
-     * @throws FinisherException
-     */
-    protected function executeInternal()
-    {
-        parent::executeInternal();
-    }
-
     /**
      * @param FormRuntime $formRuntime
      * @return StandaloneView
