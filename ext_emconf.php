3025201ecd8569e6b41caeaec7a9bcf5f71bbdb2
Hannes Lau <SomeBdyElse@users.noreply.github.com>
2020-01-24 11:20:49 +0100

    [TASK] Raise compatibility to TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
         ],
     'constraints' => [
         'depends' => [
-            'php' => '7.1.0-7.2.99',
-            'typo3' => '8.7.0-8.7.99',
+            'php' => '7.1.0-7.3.99',
+            'typo3' => '8.7.0-9.5.99',
             'extbase' => '',
         ],
         'conflicts' => [
