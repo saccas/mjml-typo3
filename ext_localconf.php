360674442936409960da29904b9df8516fa392b7
Christian Wolfram <c.wolfram@chriwo.de>
2025-05-28 13:05:05 +0200

    [TASK] Disable container register implementation
    This needs a refactoring.

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class)->registerImplementation(
-    \Saccas\Mjml\Domain\Renderer\RendererInterface::class,
-    \Saccas\Mjml\Domain\Renderer\Command::class
-);
+#\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class)->registerImplementation(
+#    \Saccas\Mjml\Domain\Renderer\RendererInterface::class,
+#    \Saccas\Mjml\Domain\Renderer\Command::class
+#);
